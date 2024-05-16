b4d788e205da498b2fda155a252de8652b33eb67
termofday <29450358+termofday@users.noreply.github.com.>
2024-05-16 17:17:02 +0200

    draw bird

--- src/Board.java
+++ src/Board.java
@@ -4,22 +4,37 @@ import javax.swing.*;
 public class Board extends JPanel {
 
     public Background bg;
-
+    public Floor floor;
+    public Pipe pipe;
+    public Bird bird;
 
 
     Board() {
         bg = new Background();
+        floor = new Floor();
+        pipe = new Pipe();
+        bird = new Bird();
     }
-
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         g.setColor(Color.RED);
+        // draw bg
         g.drawImage(bg.getImage(), 0, 0, null);
-
         g.drawImage(bg.getImage(), 288, 0, null);
+        // draw floor
+        g.drawImage(floor.getImage(), 0, 512, null);
+        g.drawImage(floor.getImage(), 288, 512, null);
+
+        // pipe
+        g.drawImage(pipe.getImage(), 288, 288, null);
+
+        // bird
+        g.drawImage(bird.getImage(), bird.x, bird.y, null);
+
+
     }
 
     
--- src/Pipe.java
+++ src/Pipe.java
@@ -1,5 +1,34 @@
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Pipe {
 
-    
+    public int x;
+    public int y;
+
+    private Image image;
+
+
+    Pipe() {
+
+        x = 500;
+        y = 0;
+        
+    }
+
+    public Image getImage() {
+
+        try {
+
+            image = ImageIO.read(new File("src/asset/sprite/pipe-green.png"));
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
     
 }
